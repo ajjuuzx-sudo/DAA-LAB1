8e055becb5af99617ccc39289f9be327d7edfeb4
ajjuuzx-sudo <ajjuuzx@gmail.com>
2026-09-24 17:44:11 +0530

    Add files via upload

---
+++ coinchange.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, amount;
+
+    cout << "Enter number of coins: ";
+    cin >> n;
+
+    int coins[n];
+
+    cout << "Enter coin values: ";
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    cout << "Enter amount: ";
+    cin >> amount;
+
+    int dp[amount + 1];
+
+    // Initialize
+    for (int i = 0; i <= amount; i++) {
+        dp[i] = amount + 1;
+    }
+
+    dp[0] = 0;
+
+    // Dynamic Programming
+    for (int i = 1; i <= amount; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (coins[j] <= i) {
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+    }
+
+    if (dp[amount] > amount)
+        cout << "Amount cannot be made";
+    else
+        cout << "Minimum coins required = " << dp[amount];
+
+    return 0;
+}
